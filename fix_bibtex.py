01cfbc669a48e3b9d2fe04058dea56de599d96ea
Hannah Mark <hannah.f.mark@gmail.com>
2023-07-03 13:40:11 -0400

    year parsing, author cleaning, doc

--- fix_bibtex.py
+++ fix_bibtex.py
@@ -1,5 +1,6 @@
 import numpy as np
 import biblib.bib as bbl
+import dateutil.parser as dp
 from habanero import Crossref
 from argparse import ArgumentParser
 from collections import Counter
@@ -39,51 +40,42 @@ fout = open('temp.bib','w')  # for doi'd entries, will overwrite if file exists
 # loop entry keys
 for key in bib_OD:
     entry = bib_OD[key]
-
-    # TODO check if there's a doi, and use it to query crossref for good ref information if so
-    if 'doi' not in entry.keys():  # query crossref and try to get a doi
+    # TODO if url us a key and doi is not, check if url is actually a doi
+    if 'doi' not in entry.keys():  # if no doi, query crossref to try and get one
         q = cr.works(query=entry['title'],query_author=entry['author'],limit=2,select='DOI,title,author')
-        print('queried for:\n%s\nby:%s\n' % (entry['title'],entry['author']))
-        print('received:\n%s\nfirst auth %s\n%s\n' % (q['message']['items'][0]['title'][0],\
+        print('\nqueried for:\ntitle: %s\nby: %s\n' % (entry['title'],entry['author']))
+        print('received:\ntitle: %s\n1st auth: %s\ndoi: %s\n' % (q['message']['items'][0]['title'][0],\
                 q['message']['items'][0]['author'][0],\
                 q['message']['items'][0]['DOI']))
-        iok = input('accept entry [Y]/n:\n') or 'Y'
+        iok = input('accept entry [Y]/n: ') or 'Y'
         if iok.lower() == 'y':
             doi = q['message']['items'][0]['DOI']
         else:
             doi = None
 
-    else:
+    else:  # if there is a doi, grab it
         if entry['doi'][-1] == '.':  # check if doi ends with . and if it does, get rid of the .
             entry['doi'] = entry['doi'][:-1]
         doi = entry['doi']
-    if doi:  # ie not None
-        # since there is a DOI, use it to query for a clean citation in case authors messed up
-        #if '(' in doi:
-        #    pre = doi[0:doi.find('(')]
-        #    suff = doi[doi.find(')')+1::]
-        #    mid = doi[doi.find('(')+1:doi.find(')')]
-        #    ourl = "https://dx.doi.org/"+pre+"\("+mid+"\)"+suff
-        #elif '. ' in doi:
+    if doi:  # if doi not none, use to query for a clean citation
         if '. ' in doi:
             ourl = "https://dx.doi.org/"+doi[0:doi.find('. ')]
         else:
             ourl = "https://dx.doi.org/"+doi
-        # now try to query crossref for it
         req = Request(ourl, headers=dict(Accept='text/bibliography; style=bibtex'))
         bibtext = urlopen(req).read().decode('utf-8')
         bibtext = bibtext.lstrip()  # clean leading space
-        # try to make a unique key; we'll fix these later
-        key_yr = re.findall('@[a-z]*{([0-9]{4})',bibtext)[0]
-        extra_letters = ''.join(random.choices(string.ascii_letters,k=5))
-        pieces = bibtext.split(key_yr)
-        start = pieces[0] + extra_letters + key_yr + pieces[1]
+        # try to make a unique bibtex key bc biblib needs unique keys to read; we'll fix these later
+        key_yr = re.findall('@[a-z]*{([0-9]{4})',bibtext)[0]  # extract auto key (should be year)
+        extra_letters = ''.join(random.choices(string.ascii_letters,k=5))  # random tag
+        pieces = bibtext.split(key_yr)  # split around key (will also split at year entry)
+        start = pieces[0] + extra_letters + key_yr + pieces[1]  # add in new bibtex key
         new_pieces = [start,]
         for p in pieces[2:]:
-            new_pieces.append(p)
-        bibtext_out = key_yr.join(new_pieces)
-        fout.write(bibtext_out)
-    else:
+            new_pieces.append(p)  # put list back together
+        bibtext_out = key_yr.join(new_pieces)  # rejoin around year entry
+        fout.write(bibtext_out)  # write
+    else:  # no doi, nothing to search with
         fout.write(entry.to_bib())  # just write what we had to start with
         fout.write('\n')
 
@@ -99,21 +91,6 @@ parsed = parser.parse(open('temp.bib','r'))  # parse the input file with biblib
 # get entries
 bib_OD = parsed.get_entries()  # returns collections.OrderedDict
 
-# check if all the titles end with an apostrophe; if so, probably an error and they need to 
-# be removed (probably quoted titles and anystyle only removed the leading quote)
-# this may not be an issue anymore since we should get clean titles from crossref mostly?
-# TODO deal with this for things that are not queried
-last_char = np.array([bib_OD[key]['title'][-1] for key in bib_OD])
-strp_last = False
-lc = Counter(last_char)
-if max(lc.values()) >= len(last_char)/2:  # this is a red flag
-    print('title trailing characters:')
-    for i,k in enumerate(lc.keys()):
-        print('[%i] %i instances of %s' % (i,lc[k],k))
-    irem = int(input('enter index to remove: ') or -1)
-    if irem >= 0: 
-        strp_last = True  # and we'll remove the one with that index in the keys list
-        char_out = list(lc.keys())[irem]
 
 # open outfile for writing
 fout = open(out_bib,'w')  # will overwrite if file exists
@@ -129,42 +106,44 @@ for key in bib_OD:
     if 'date' in entry.keys():
         if len(entry['date']) == 4: # assume this means it's a year, which is probably true
             entry['year'] = entry['date']
-        else: # possibly 'month year'? or some other format, check on that
-            entry['year'] = entry['date'].split(' ')[-1]
-            if len(entry['year']) != 4:
-                print('date is %s, year set to %s' % (entry['date'],entry['year']))
-                iy = input('enter corrected year if needed: ') or None
-                if iy != None:
-                    entry['year'] = iy
+        else: # some other format, try to parse
+            try_year = dp.parse(entry['date']).year
+            print('date is %s, year set to %s' % (entry['date'],try_year))
+            iy = input('enter corrected year if needed: ') or None
+            if iy != None:
+                entry['year'] = iy
     if 'url' in entry.keys():
         if re.search(r"doi\.org",entry['url']):  # check if url is just a doi link
-            _ = entry.pop('url')
-# TODO if there is a url but no doi and the url is a doi, repurpose
+            _ = entry.pop('url')  # if it is, we should have used it earlier and can clean now
 
-#    if 'pages' in entry.keys():
+    # TODO decide if pages should ever be kept
+    # tends to be a messy field; sometimes an id#, sometimes page range, sometimes number of pages
+#    if 'pages' in entry.keys():  
 #        _ = entry.pop('pages')
 
     if 'pages' in entry.keys():
-        entry['pages'] = entry['pages'].rstrip(',')  # get rid of any trailing commas
+        entry['pages'] = entry['pages'].rstrip(',')  # at least get rid of any trailing commas
 
     if 'note' in entry.keys():
         entry['note'] = re.sub(r'Available at','',entry['note'])
         if len(entry['note']) <= 3:  # if we've only got 3 characters left or less, probably nothing
             _ = entry.pop('note')  # should take care of case with : at the end
 
-    if strp_last:
-        if entry['title'].endswith(char_out):
-            entry['title'] = entry['title'][:-1]  # remove trailing apostrophe (probably)
+    if entry['title'].endswith("'"):
+        entry['title'] = entry['title'][:-1]  # remove trailing apostrophe if present
+
+    # clean up first author name: no {}, no spaces
+    auth0 = re.sub(r'[ ]',r'',entry.authors()[0].last.lstrip('{').rstrip('}'))
 
     # for each, reformat key to be what we'd look for in inline citations
     # first, count authors: if >2, key is firstauthorEAYYYY, if 2 or less is author(author)YYYY
-    # TODO deal with {} in author lists if it comes up
     if len(entry.authors()) > 2:
-        newkey = entry.authors()[0].last + 'EA' + entry['year']
+        newkey = auth0 + 'EA' + entry['year']
     elif len(entry.authors()) == 2:
-        newkey = entry.authors()[0].last + entry.authors()[1].last + entry['year']
+        auth1 = re.sub(r'[ ]',r'',entry.authors()[1].last.lstrip('{').rstrip('}'))
+        newkey = auth0 + auth1 + entry['year']
     elif len(entry.authors()) == 1:
-        newkey = entry.authors()[0].last + entry['year']
+        newkey = auth0 + entry['year']
     newkey = newkey.replace(" ","")  # get rid of spaces if there are any (like van Keken or something)
     newkey = newkey.replace("-","")  # get rid of hyphens, which tex2jats doesn't like
     nextletter = 'a'
